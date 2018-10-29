46c615a06789c26e6db5f9ba6527811320f363a6
Cloving <quds8724@qq.com>
2018-10-29 21:54:24 +0800

    update

--- qiuShiBaiKe.py
+++ qiuShiBaiKe.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import urllib.request
+import urllib.error
 import re
 
 
@@ -13,6 +14,45 @@ class QSBK:
     self.stories = []
     self.enable = False
 
+  def getPage(self, pageIndex):
+    try:
+      url = 'https://www.qiushibaike.com/hot/page/' + str(pageIndex)
+      user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) ' \
+             'AppleWebKit/537.36 (KHTML, like Gecko) ' \
+             'Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134'
+      headers = {'User-Agent': user_agent}
+      request = urllib.request.Request(url, headers)
+      response = urllib.request.urlopen(request)
+      pageCode = response.read().decode('utf8')
+      return pageCode
+    except(urllib.error.URLError, urllib.error.HTTPError) as e:
+      if hasattr(e, "reason"):
+        print(u"连接错误，错误原因", e.reason)
+        return None
+
+  def getPageItems(self, pageIndex):
+    pageCode = self.getPage(pageIndex)
+    if not pageCode:
+      print(u"页面加载失败")
+      return None
+    pattern = re.compile('<div.*?class="author.*?>.*?<a.*?>.*?</a>.*?<a.*?>.*?<h2>(.*?)</h2>.*?'
+                         '<div.*?class="articleGender.*?>(.*?)</div>.*?'
+                         '<div.*?class="content.*?>.*?<span>(.*?)</span>.*?'
+                         '<span.*?class="stats-vote.*?>.*?<i.*?>(.*?)</i>', re.S)
+    items = re.findall(pattern, pageCode)
+    pageStories = []
+    for item in items:
+      pageStories.append(item[0].strip(), item[1].strip(), item[2].strip().replace('<br/>', '\n'), item[3].strip())
+    return pageStories
+
+  def loadPage(self):
+    if self.enable:
+      if len(self.stories) < 2:
+        pageStories = self.getPageItems(self.pageIndex)
+        if pageStories:
+          self.stories.append(pageStories)
+          self.pageIndex += 1
+
   def startSearch(self):
     print('Reading... Press Q quit')
     self.enable = True
@@ -25,6 +65,7 @@ class QSBK:
         del self.stories[0]
         self.getOneStory(pageStories, curPage)
 
+
 spider = QSBK()
-spider.startIndex()
+spider.startSearch()
 
