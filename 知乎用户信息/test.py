46c0cfe3dd3e9a9929cff5474250c5d45f37f24c
Cloving <quds8724@qq.com>
2018-12-17 16:05:36 +0800

    update

--- 知乎用户信息/test.py
+++
@@ -1,9 +0,0 @@
-from pyecharts import Map
-
-value = [155, 10, 66, 78]
-attr = {"福建": "山东", "北京": "上海"}
-# map = Map("全国地图示例", width=1200, height=600)
-# map.add("", attr, value, maptype='china')
-# map.render()
-
-print(list(attr.items()))
